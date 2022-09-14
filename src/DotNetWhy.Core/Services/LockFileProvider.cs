ba7d7b8db047dc8eca1e65cdf7070793ef3922f5
Tomasz Adamczyk <tomekadamczyknc@gmail.com>
2022-09-14 17:00:46 +0200

    Moved restoring projects so it runs only once (#33)

--- src/DotNetWhy.Core/Services/LockFileProvider.cs
+++ src/DotNetWhy.Core/Services/LockFileProvider.cs
@@ -3,32 +3,16 @@
 internal class LockFileProvider : ILockFileProvider
 {
     private readonly ILockFileSourceProvider _sourceProvider;
-    private readonly RetryHelper _retry = new();
 
     public LockFileProvider(ILockFileSourceProvider sourceProvider)
     {
         _sourceProvider = sourceProvider;
     }
 
-    public LockFile Get(
-        string workingDirectory,
-        string outputDirectory)
+    public LockFile Get(string outputDirectory)
     {
         var lockFileSource = _sourceProvider.Get(outputDirectory);
 
-        try
-        {
-            var dotNetRestoreResult = DotNetRunner.Restore(workingDirectory);
-
-            return dotNetRestoreResult.IsSuccess
-                ? LockFileUtilities.GetLockFile(lockFileSource, NullLogger.Instance)
-                : throw new RestoreProjectFailedException(workingDirectory);
-        }
-        catch (Exception exception) when (exception is not RestoreProjectFailedException)
-        {
-            return _retry.CanTryAgain()
-                ? Get(workingDirectory, outputDirectory)
-                : throw new RestoreProjectFailedException(workingDirectory);
-        }
+        return LockFileUtilities.GetLockFile(lockFileSource, NullLogger.Instance);
     }
 }
